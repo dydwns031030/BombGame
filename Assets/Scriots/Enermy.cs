654ad54e6410ef8a181ac45f89fd6bb4327e2fc6
dydwns031030 <80015333+dydwns031030@users.noreply.github.com>
2021-06-01 11:33:48 +0900

    6/1Gun

--- Assets/Scriots/Enermy.cs
+++ Assets/Scriots/Enermy.cs
@@ -7,6 +7,9 @@ public class Enermy : MonoBehaviour
     public Transform target;
     private Rigidbody rig;
     public float speed = 20f;
+
+    private bool scored = false;
+
     private void Start()
     {
         rig = GetComponent<Rigidbody>();
@@ -19,5 +22,14 @@ public class Enermy : MonoBehaviour
             Vector3 trans = target.position - transform.position;
             rig.AddForce(trans * speed * Time.deltaTime);
         }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Bullet" && scored == false)
+        {
+            scored = true; GameManager.instance.AddScore(1);
+            transform.position = new Vector3(Random.Range(-30, 30), 0.5f, Random.Range(-30, 30));
+            scored = false; 
+        } 
     }
 }
