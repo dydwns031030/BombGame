405e69f6d3ce9fb83794f41b473fbe6ecfa37883
dydwns031030 <80015333+dydwns031030@users.noreply.github.com>
2021-06-11 09:43:47 +0900

    6/11

---
+++ Assets/Scripts/Enermy.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enermy : MonoBehaviour
+{
+    public Transform target;
+    private Rigidbody rig;
+    public float speed = 20f;
+   
+    public AudioSource enermyAudio; 
+    public GameObject explosionEffect;
+
+    private bool scored = false;
+
+    void Start()
+    {
+        enermyAudio = GetComponent<AudioSource>();
+        rig = GetComponent<Rigidbody>();
+        target = GameObject.FindWithTag("Player").transform;
+    }
+    void Update()
+    {
+        if(Vector3.Distance(target.position, transform.position) < 10f)
+        {
+            Vector3 trans = target.position - transform.position;
+            rig.AddForce(trans * speed * Time.deltaTime);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Bullet" && scored == false)
+        {
+            enermyAudio.Play();
+            Instantiate(explosionEffect, transform.position, transform.rotation);
+            scored = true;
+            GameManager.instance.AddScore(1);
+            transform.position = new Vector3(Random.Range(-30, 30), 0.5f, Random.Range(-30, 30));
+            scored = false;
+        }
+    }
+}
